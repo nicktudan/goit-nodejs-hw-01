cd7ab162afd19a3fe2688caf4e02b4575197cb4a
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-05-07 20:46:11 +0200

    Add yarn and command options

--- contacts.js
+++ contacts.js
@@ -36,9 +36,9 @@ const removeContact = async (id) => {
   if (index === -1) {
     return null;
   }
-  const [result] = contacts.splice(index, 1);
+  const [deleteContact] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
+  return deleteContact;
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -16,7 +29,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       const deleteContact = await contacts.removeContact(id);
       return console.log(deleteContact);
     default:
-      console.warn("Unknown action...");
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
